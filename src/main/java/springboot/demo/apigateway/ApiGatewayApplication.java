d8110909b05d1026fd54321a4960ffb3cbc1ad07
sunghoan-we <javarajava@hotmail.com>
2023-10-04 11:00:07 -0700

    modify package name

--- src/main/java/springboot/demo/apigateway/ApiGatewayApplication.java
+++ src/main/java/springboot/demo/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package springboot.demo.apiGateway;
+package springboot.demo.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/springboot/demo/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/springboot/demo/apigateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package springboot.demo.apiGateway;
+package springboot.demo.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
